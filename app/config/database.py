7f06cf9ce8f9f47d101c71aa5768ebd25089085c
Bartlomiej Kopysc <bkopysc@wp.pl>
2024-04-02 18:57:24 +0200

    env options

--- app/config/database.py
+++ app/config/database.py
@@ -9,15 +9,21 @@ import os
 
 load_dotenv()
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+USE_SQLITE_DB = os.getenv("USE_SQLITE_DB") == "True"
+
+print(USE_SQLITE_DB)
 
 """FOR SQLITE DATABASE"""
-# engine = create_engine("sqlite:///example.db")
-
-# def create_tables():
-#     Base.metadata.create_all(bind=engine)
+if(USE_SQLITE_DB):
+    engine = create_engine("sqlite:///example.db")
 
 """FOR OTHER DATABASES"""
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+if(not USE_SQLITE_DB):
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+"""CREATE TABLES"""
+def create_tables():
+    Base.metadata.create_all(bind=engine)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
@@ -29,18 +35,21 @@ def get_db():
     finally:
         db.close()
 
-"""PRE-INIT DATABASE"""
-
 
 """PRE-PING DATABASE"""
 
 try:
     db = SessionLocal()
-    #create_tables() # TODO: REMOVE THIS LINE IN PRODUCTION
-    init_db_data(db) # TODO: REMOVE THIS LINE IN PRODUCTION
+    if(USE_SQLITE_DB):
+        create_tables() # TODO: REMOVE THIS LINE IN PRODUCTION
+    #init_db_data(db) # TODO: REMOVE THIS LINE IN PRODUCTION
     db.execute(text('SELECT 1'))
     db.close()
     print("DATABASE IS READY")
+    if(USE_SQLITE_DB):
+        print("DB DEBUG")
+    else:
+        print("DB PRODUCTION")
 except Exception as e:
     print("DATABASE IS NOT READY")
     print(e)
